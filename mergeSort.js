137208792c81d324228adf4108302ee20d92b123
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-05-25 13:02:46 +0530

    merge sort

---
+++ mergeSort.js
@@ -0,0 +1,24 @@
+function merge(leftArr, rightArr){
+    let sortedArr = [];
+    while(leftArr.length && rightArr.length){
+        if(leftArr[0]<= rightArr[0]){
+            sortedArr.push(leftArr.shift());
+        } else{
+            sortedArr.push(rightArr.shift());
+        }
+    }
+    return sortedArr;
+}
+
+function mergeSort(arr){
+    if(arr.length < 2){
+        return arr;
+    }
+    let mid = Math.floor(arr.length/2);
+    let leftArr = arr.slice(0,mid);
+    let rightArr = arr.slice(mid);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+const arr = [2,7,3,9,4,1];
+console.log(mergeSort(arr));
